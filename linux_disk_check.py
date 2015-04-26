6377133e708c1506c6596afb208d8a468e605e33
Sinkemlow <jamesalanthompson@gmail.com>
2015-04-26 10:04:32 +0100

    PyCharm bits and bobs.

---
+++ linux_disk_check.py
@@ -0,0 +1,20 @@
+from subprocess import check_output
+
+df = check_output(['df', '-Ph'])
+df_output = df.split('\n')
+df_output.pop(0)
+df_output.pop()
+
+def disk_check(df_list):
+    print "----------------------"
+    print "Filesystems above 80%:"
+    print "----------------------"
+    for line in df_list:
+        usage = line.split()[4]
+        filesystem = line.split()[5]
+        percentage = int(usage.split("%")[0])
+    
+        if percentage >= 80:
+            print "The %s filesystem is at %s%%" % (filesystem, percentage)
+    
+disk_check(df_output)
